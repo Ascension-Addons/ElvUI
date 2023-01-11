5cdc2b2b9a5911d924a8056a51863802475a0d16
Anthony Narkevicius <thebbandit@gmail.com>
2023-01-11 10:06:04 -0800

    add icon size option to menu

--- ElvUI_OptionsUI/Maps.lua
+++ ElvUI_OptionsUI/Maps.lua
@@ -150,6 +150,21 @@ E.Options.args.maps = {
 							set = function(info, value) E.db.general.mapMarkers[info[#info]] = value E:StaticPopup_Show("PRIVATE_RL") end,
 							disabled = function() return not E.db.general.mapMarkers.enable end,
 						},
+						spacer = {
+							order = 3,
+							type = "description",
+							name = " "
+						},
+						iconSize = {
+							order = 4,
+							type = "range",
+							name = L["Icon Size"],
+							desc = L["Set the size of the map markers"],
+							min = 15, max = 50, step = 1,
+							get = function(info) return E.db.general.mapMarkers[info[#info]] end,
+							set = function(info, value) E.db.general.mapMarkers[info[#info]] = value E:StaticPopup_Show("PRIVATE_RL") end,
+							disabled = function() return not E.db.general.mapMarkers.enable end,
+						},
 					}
 				}
 			}
