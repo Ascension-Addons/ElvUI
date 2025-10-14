85f6dfe52a432dbdc02b20ea058aa859e89d572b
andrew6180 <16847730+andrew6180@users.noreply.github.com>
2025-10-13 22:54:14 -0700

    add reborn spell to range checks. fixes #80

--- ElvUI/Settings/Global.lua
+++ ElvUI/Settings/Global.lua
@@ -46,91 +46,118 @@ G.unitframe = {
 		PRIEST = {
 			enemySpells = {
 				[585] = true, -- Smite (30 yards)
+				[1100585] = true, -- Smite (30 yards)
 			},
 			longEnemySpells = {
 				[589] = true, -- Shadow Word: Pain (30 yards)
+				[1100589] = true, -- Shadow Word: Pain (30 yards)
 			},
 			friendlySpells = {
 				[2050] = true, -- Lesser Heal (40 yards)
+				[1102050] = true, -- Lesser Heal (40 yards)
 			},
 			resSpells = {
 				[2006] = true, -- Resurrection (40 yards)
+				[1102006] = true, -- Resurrection (40 yards)
 			},
 			petSpells = {},
 		},
 		DRUID = {
 			enemySpells = {
 				[33786] = true, -- Cyclone (20 yards)
+				[1133786] = true, -- Cyclone (20 yards)
 			},
 			longEnemySpells = {
 				[5176] = true, -- Wrath (30 yards)
+				[1105176] = true, -- Wrath (30 yards)
 			},
 			friendlySpells = {
 				[5185] = true, -- Healing Touch (40 yards)
+				[1105185] = true, -- Healing Touch (40 yards)
 			},
 			resSpells = {
 				[50769] = true, -- Revive (30 yards)
+				[1150769] = true, -- Revive (30 yards)
 				[20484] = true, -- Rebirth (30 yards)
+				[1120484] = true, -- Rebirth (30 yards)
 			},
 			petSpells = {},
 		},
 		PALADIN = {
 			enemySpells = {
 				[20271] = true, -- Judgement (10 yards)
+				[1120271] = true, -- Judgement (10 yards)
 			},
 			longEnemySpells = {
 				[879] = true, -- Exorcism (30 yards)
+				[1100879] = true, -- Exorcism (30 yards)
 			},
 			friendlySpells = {
 				[635] = true, -- Holy Light (40 yards)
+				[1100635] = true, -- Holy Light (40 yards)
 			},
 			resSpells = {
 				[7328] = true, -- Redemption (30 yards)
+				[1107328] = true, -- Redemption (30 yards)
 			},
 			petSpells = {},
 		},
 		SHAMAN = {
 			enemySpells = {
 				[51514] = true, -- Hex (20 yards)
+				[1151514] = true, -- Hex (20 yards)
 				[8042] = true, -- Earth Shock (25 yards)
+				[1108042] = true, -- Earth Shock (25 yards)
 			},
 			longEnemySpells = {
 				[403] = true, -- Lightning Bolt (30 yards)
+				[1100403] = true, -- Lightning Bolt (30 yards)
 			},
 			friendlySpells = {
 				[331] = true, -- Healing Wave (40 yards)
+				[1100331] = true, -- Healing Wave (40 yards)
 			},
 			resSpells = {
 				[2008] = true, -- Ancestral Spirit (30 yards)
+				[1102008] = true, -- Ancestral Spirit (30 yards)
 			},
 			petSpells = {},
 		},
 		WARLOCK = {
 			enemySpells = {
 				[5782] = true, -- Fear (20 yards)
+				[1105782] = true, -- Fear (20 yards)
 			},
 			longEnemySpells = {
 				[686] = true, -- Shadow Bolt (30 yards)
+				[1100686] = true, -- Shadow Bolt (30 yards)
 			},
 			friendlySpells = {
 				[5697] = true, -- Unending Breath (30 yards)
+				[1105697] = true, -- Unending Breath (30 yards)
 			},
 			resSpells = {},
 			petSpells = {
 				[755] = true, -- Health Funnel (45 yards)
+				[1100755] = true, -- Health Funnel (45 yards)
 			},
 		},
 		MAGE = {
 			enemySpells = {
 				[2136] = true, -- Fire Blast (20 yards)
+				[1102136] = true, -- Fire Blast (20 yards)
 				[12826] = true, -- Polymorph (30 yards)
+				[1112826] = true, -- Polymorph (30 yards)
 			},
 			longEnemySpells = {
 				[133] = true, -- Fireball (35 yards)
+				[1100133] = true, -- Fireball (35 yards)
 				[44614] = true, -- Frostfire Bolt (40 yards)
+				[1144614] = true, -- Frostfire Bolt (40 yards)
 			},
 			friendlySpells = {
 				[475] = true, -- Remove Curse (40 yards)
+				[1100475] = true, -- Remove Curse (40 yards)
 			},
 			resSpells = {},
 			petSpells = {},
@@ -138,36 +165,44 @@ G.unitframe = {
 		HUNTER = {
 			enemySpells = {
 				[75] = true, -- Auto Shot (35 yards)
+				[1100075] = true, -- Auto Shot (35 yards)
 			},
 			longEnemySpells = {},
 			friendlySpells = {},
 			resSpells = {},
 			petSpells = {
 				[136] = true, -- Mend Pet (45 yards)
+				[1100136] = true, -- Mend Pet (45 yards)
 			},
 		},
 		DEATHKNIGHT = {
 			enemySpells = {
 				[49576] = true, -- Death Grip (30 yards)
+				[1149576] = true, -- Death Grip (30 yards)
 			},
 			longEnemySpells = {},
 			friendlySpells = {
 				[47541] = true, -- Death Coil (40 yards)
+				[1147541] = true, -- Death Coil (40 yards)
 			},
 			resSpells = {
 				[61999] = true, -- Raise Ally (30 yards)
+				[1161999] = true, -- Raise Ally (30 yards)
 			},
 			petSpells = {},
 		},
 		ROGUE = {
 			enemySpells = {
 				[2094] = true, -- Blind (10 yards)
+				[1102094] = true, -- Blind (10 yards)
 			},
 			longEnemySpells = {
 				[26679] = true, -- Deadly Throw (30 yards)
+				[1126679] = true, -- Deadly Throw (30 yards)
 			},
 			friendlySpells = {
 				[57934] = true, -- Tricks of the Trade (20 yards)
+				[1157934] = true, -- Tricks of the Trade (20 yards)
 			},
 			resSpells = {},
 			petSpells = {},
@@ -175,13 +210,17 @@ G.unitframe = {
 		WARRIOR = {
 			enemySpells = {
 				[5246] = true, -- Intimidating Shout (8 yards)
+				[1105246] = true, -- Intimidating Shout (8 yards)
 				[100] = true, -- Charge (25 yards)
+				[1100100] = true, -- Charge (25 yards)
 			},
 			longEnemySpells = {
 				[355] = true, -- Taunt (30 yards)
+				[1100355] = true, -- Taunt (30 yards)
 			},
 			friendlySpells = {
 				[3411] = true, -- Intervene (25 yards)
+				[1103411] = true, -- Intervene (25 yards)
 			},
 			resSpells = {},
 			petSpells = {},
